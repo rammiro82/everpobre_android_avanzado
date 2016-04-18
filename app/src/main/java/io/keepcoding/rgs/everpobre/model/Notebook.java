e5f82d756278eb878e7fceb413df10a17ed2ac18
Ramiro García Salazar <rammiro82@gmail.com>
2016-04-18 22:27:39 +0200

    Created initial version of Notebook class

---
+++ app/src/main/java/io/keepcoding/rgs/everpobre/model/Notebook.java
@@ -0,0 +1,26 @@
+package io.keepcoding.rgs.everpobre.model;
+
+/**
+ * Created by RamiroGarcia on 18/4/16.
+ */
+public class Notebook {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
