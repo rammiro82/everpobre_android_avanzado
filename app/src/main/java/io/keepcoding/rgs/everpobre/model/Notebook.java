4ea9d7ee6a6a0d0cd8208cee3fe3e9b11fd40fea
Ramiro García Salazar <rammiro82@gmail.com>
2016-04-18 23:03:50 +0200

    Added Constructors.

--- app/src/main/java/io/keepcoding/rgs/everpobre/model/Notebook.java
+++ app/src/main/java/io/keepcoding/rgs/everpobre/model/Notebook.java
@@ -8,6 +8,15 @@ public class Notebook {
     private int id;
     private String name;
 
+    private Notebook() {
+        // disable default constructor.
+    }
+
+    public Notebook(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
