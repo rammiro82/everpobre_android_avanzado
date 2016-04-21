b84b7728849da9622c4db66ca36c94b11a421189
Ramiro García Salazar <RamiroGarcia@Ramiro-Air.local>
2016-04-22 00:17:21 +0200

    adapter, views and fragments creation.

---
+++ app/src/main/java/io/keepcoding/rgs/everpobre/views/NotebookRowViewHolder.java
@@ -0,0 +1,29 @@
+package io.keepcoding.rgs.everpobre.views;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import io.keepcoding.rgs.everpobre.R;
+
+/**
+ * Created by RamiroGarcia on 21/4/16.
+ */
+public class NotebookRowViewHolder extends RecyclerView.ViewHolder {
+
+    private String notebookName;
+
+    @Bind(R.id.row_notebook_notebook_name) TextView notebookNameTextView;
+
+    public NotebookRowViewHolder(View rowNotebook) {
+        super(rowNotebook);
+
+        ButterKnife.bind(rowNotebook);
+    }
+
+    public void setNotebookName (String name){
+        notebookNameTextView.setText(name);
+    }
+}
