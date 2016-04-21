4c33d554ae212bc8a0ea792b69015c241cada677
Ramiro García Salazar <RamiroGarcia@Ramiro-Air.local>
2016-04-21 21:52:47 +0200

    Refactor notebookDAO, finish query methods initiazliced db

--- app/src/main/java/io/keepcoding/rgs/everpobre/model/db/DBHelper.java
+++ app/src/main/java/io/keepcoding/rgs/everpobre/model/db/DBHelper.java
@@ -1,5 +1,6 @@
 package io.keepcoding.rgs.everpobre.model.db;
 
+import java.lang.ref.WeakReference;
 import java.util.Date;
 
 import android.content.Context;
@@ -7,30 +8,38 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import io.keepcoding.rgs.everpobre.model.db.dao.NotebookDAO;
-
 public class DBHelper extends SQLiteOpenHelper {
 
 	public static final int DATABASE_VERSION = 1;
 
 	public static final String[] CREATE_DATABASE_SCRIPTS = {
-			NotebookDAO.SQL_CREATE_NOTEBOOK_TABLE
+			DBConstants.SQL_CREATE_NOTEBOOK_TABLE
 	};
 	
-	public static final String DROP_DATABASE = "";
+	public static final String DROP_DATABASE = "ALTER TABLE ...";
 
 	private static DBHelper sInstance;
+	private static String dbName;
+	private static WeakReference<Context> weakContext;
 	
 	public DBHelper(String databaseName, Context context) {
 		super(context, databaseName, null, DATABASE_VERSION);
 	}
 
-	public static DBHelper getInstance(String databaseName, Context context) {
+	public static void configure (String databaseName, Context context){
+		dbName = databaseName;
+		weakContext = new WeakReference<Context>(context);
+	}
+
+	public static DBHelper getInstance() {
+		if(dbName == null || weakContext == null){
+			throw new IllegalStateException("No database provide, no context");
+		}
 	    // Use the application context, which will ensure that you 
 	    // don't accidentally leak an Activity's context.
 	    // See this article for more information: http://bit.ly/6LRzfx
 	    if (sInstance == null) {
-	      sInstance = new DBHelper(databaseName, context.getApplicationContext());
+	      sInstance = new DBHelper(dbName, weakContext.get().getApplicationContext());
 	    }
 	    return sInstance;
 	 }
--- app/src/main/java/io/keepcoding/rgs/everpobre/model/db/dao/NotebookDAO.java
+++ app/src/main/java/io/keepcoding/rgs/everpobre/model/db/dao/NotebookDAO.java
@@ -2,44 +2,28 @@ package io.keepcoding.rgs.everpobre.model.db.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.lang.ref.WeakReference;
+import java.util.LinkedList;
+import java.util.List;
 
 import io.keepcoding.rgs.everpobre.model.Notebook;
+import io.keepcoding.rgs.everpobre.model.Notebooks;
+import io.keepcoding.rgs.everpobre.model.db.DBConstants;
 import io.keepcoding.rgs.everpobre.model.db.DBHelper;
 
 public class NotebookDAO {
-	public static final String TABLE_NOTEBOOK = "NOTEBOOK";
-
-	// Table field constants 
-	public static final String KEY_NOTEBOOK_ID = "_id";
-	public static final String KEY_NOTEBOOK_NAME = "name";
-
-	public static final String SQL_CREATE_NOTEBOOK_TABLE =
-			"create table " + TABLE_NOTEBOOK
-					+ "( " + KEY_NOTEBOOK_ID
-					+ " integer primary key autoincrement, "
-					+ KEY_NOTEBOOK_NAME + " text not null"
-					+ ");";
 
 
-	public static final String[] allColumns = {
-			KEY_NOTEBOOK_ID,
-			KEY_NOTEBOOK_NAME
-	};
-	private static final long INVALID_ID_DELETE_ALL_RECORDS = 0;
-	private final String databaseName;
+    private static final long INVALID_ID_DELETE_ALL_RECORDS = 0;
 
-	private WeakReference<Context> context;
-	// private Context context;
+    private DBHelper db;
 
-	public NotebookDAO(Context context) {
-		this("NOTEBOOK.sqlite", context);
-	}
-
-	public NotebookDAO(String databaseName, Context context) {
-		this.context = new WeakReference<Context>(context);
-		this.databaseName = databaseName;
+	public NotebookDAO() {
+        db = DBHelper.getInstance();
 	}
 
 	public long insert(Notebook notebook) {
@@ -47,14 +31,7 @@ public class NotebookDAO {
 			throw new IllegalArgumentException("Passing NULL NOTEBOOK, imbecile");
 		}
 
-		if (context.get() == null) {
-			throw new IllegalStateException("Context NULL");
-		}
-
-		// insert
-		DBHelper db = DBHelper.getInstance(this.databaseName, context.get());
-
-		long id = db.getWritableDatabase().insert(TABLE_NOTEBOOK, null, this.getContentValues(notebook));
+		long id = db.getWritableDatabase().insert(DBConstants.TABLE_NOTEBOOK, null, this.getContentValues(notebook));
 		notebook.setId(id);
 		db.close();
 		db=null;
@@ -69,15 +46,10 @@ public class NotebookDAO {
 		if (id < 1) {
 			throw new IllegalStateException("Passing id imbecile");
 		}
-		if (context.get() == null) {
-			throw new IllegalStateException("Context NULL");
-		}
-
-		DBHelper db = DBHelper.getInstance(this.databaseName, context.get());
 
         // two diferents ways of doing the same.
 		//int numberOfRowsUpdated = db.getWritableDatabase().update(TABLE_NOTEBOOK, this.getContentValues(notebook), KEY_NOTEBOOK_ID + "=" + id, null);
-		int numberOfRowsUpdated = db.getWritableDatabase().update(TABLE_NOTEBOOK, this.getContentValues(notebook), KEY_NOTEBOOK_ID + "=?", new String[]{"" + id});
+		int numberOfRowsUpdated = db.getWritableDatabase().update(DBConstants.TABLE_NOTEBOOK, this.getContentValues(notebook), DBConstants.KEY_NOTEBOOK_ID + "=?", new String[]{"" + id});
 
 		db.close();
 		db=null;
@@ -85,12 +57,11 @@ public class NotebookDAO {
 	}
 
 	public void delete(long id) {
-		DBHelper db = DBHelper.getInstance(this.databaseName, context.get());
 
 		if (id == INVALID_ID_DELETE_ALL_RECORDS) {
-			db.getWritableDatabase().delete(TABLE_NOTEBOOK,  null, null);
+			db.getWritableDatabase().delete(DBConstants.TABLE_NOTEBOOK,  null, null);
 		} else {
-			db.getWritableDatabase().delete(TABLE_NOTEBOOK, KEY_NOTEBOOK_ID + " = " + id, null);
+			db.getWritableDatabase().delete(DBConstants.TABLE_NOTEBOOK, DBConstants.KEY_NOTEBOOK_ID + " = " + id, null);
 		}
 		db.close();
 		db=null;
@@ -103,7 +74,7 @@ public class NotebookDAO {
 	public static ContentValues getContentValues(Notebook notebook) {
 		ContentValues content = new ContentValues();
 
-		content.put(KEY_NOTEBOOK_NAME, notebook.getName());
+		content.put(DBConstants.KEY_NOTEBOOK_NAME, notebook.getName());
 
 		return content;
 	}
@@ -111,70 +82,78 @@ public class NotebookDAO {
 
 	// convenience method
 
-/*	public static Notebook notebookFromCursor(Cursor c) {
+	public static Notebook elementFromCursor(final @NonNull Cursor c) {
 		assert c != null;
 
-		String name = c.getString(c.getColumnIndex(KEY_NOTEBOOK_NAME));
-		//String description = c.getString(c.getColumnIndex(KEY_NOTEBOOK_DESCRIPTION));
-		long id = c.getLong(c.getColumnIndex(KEY_NOTEBOOK_ID));
+		String name = c.getString(c.getColumnIndex(DBConstants.KEY_NOTEBOOK_NAME));
+		long id = c.getLong(c.getColumnIndex(DBConstants.KEY_NOTEBOOK_ID));
 
-		Notebook notebook = new Notebook(name);
+		Notebook notebook = new Notebook(id, name);
 		notebook.setId(id);
 
 		return notebook;
 	}
 
-*/
+
 	/**
 	 * Returns all NOTEBOOKs in DB inside a Cursor
 	 * @return cursor with all records
 	 */
-/*	public Cursor queryCursor() {
+	public Cursor queryCursor() {
 		// select
-		DBHelper db = DBHelper.getInstance(this.databaseName, context.get());
 
-		Cursor c = db.getReadableDatabase().query(TABLE_NOTEBOOK, allColumns, null, null, null, null, null);
+		Cursor c = db.getReadableDatabase().query(DBConstants.TABLE_NOTEBOOK,
+                DBConstants.allColumns,
+                null,
+                null,
+                null,
+                null,
+                DBConstants.KEY_NOTEBOOK_ID);
+
+
 
 		return c;
 	}
 
-	public NOTEBOOKs query() {
-		NOTEBOOKs NOTEBOOKs = new NOTEBOOKs();
+	public Notebooks query() {
+
+        List<Notebook> notebookList = new LinkedList<>();
 
 		Cursor cursor = queryCursor();
 		if (cursor != null && cursor.getCount() > 0) {
 			cursor.moveToFirst();
 			do {
-				NOTEBOOK NOTEBOOK = NOTEBOOKFromCursor(cursor);
-				NOTEBOOKs.add(NOTEBOOK);
+				Notebook notebook = elementFromCursor(cursor);
+                notebookList.add(notebook);
 			} while (cursor.moveToNext());
 		}
-		return NOTEBOOKs;
+
+        Notebooks notebooks = Notebooks.createNotebooks(notebookList);
+
+		return notebooks;
 	}
-*/
+
 
 	/**
 	 * Returns a NOTEBOOK object from its id
 	 * @param id - the NOTEBOOK id in db
 	 * @return NOTEBOOK object if found, null otherwise
 	 */
-/*	public NOTEBOOK query(long id) {
-		NOTEBOOK NOTEBOOK = null;
+	public @Nullable Notebook query(long id) {
+        Notebook notebook = null;
 
-		DBHelper db = DBHelper.getInstance(this.databaseName, context.get());
-
-		String where = KEY_NOTEBOOK_ID + "=" + id;
-		Cursor c = db.getReadableDatabase().query(TABLE_NOTEBOOK, allColumns, where, null, null, null, null);
+		String where = DBConstants.KEY_NOTEBOOK_ID + "=" + id;
+		Cursor c = db.getReadableDatabase().query(DBConstants.TABLE_NOTEBOOK, DBConstants.allColumns, where, null, null, null, null);
 		if (c != null) {
 			if (c.getCount() > 0) {
 				c.moveToFirst();
-				NOTEBOOK = NOTEBOOKFromCursor(c);
+                notebook = elementFromCursor(c);
 			}
+            c.close();
 		}
-		c.close();
 		db.close();
-		return NOTEBOOK;
+		return notebook;
 	}
 
-*/
+
 }
